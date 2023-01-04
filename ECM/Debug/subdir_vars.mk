8e47b618c13eeb0248f58849a72a784b6b7f4a21
diomig <114738814+diomig@users.noreply.github.com>
2023-01-04 17:30:26 +0000

    Add files via upload

---
+++ ECM/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../blinky_ccs.cmd 
+
+C_SRCS += \
+../EngineControl.c \
+../lcd.c \
+../startup_ccs.c 
+
+C_DEPS += \
+./EngineControl.d \
+./lcd.d \
+./startup_ccs.d 
+
+OBJS += \
+./EngineControl.obj \
+./lcd.obj \
+./startup_ccs.obj 
+
+OBJS__QUOTED += \
+"EngineControl.obj" \
+"lcd.obj" \
+"startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"EngineControl.d" \
+"lcd.d" \
+"startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../EngineControl.c" \
+"../lcd.c" \
+"../startup_ccs.c" 
+
+
